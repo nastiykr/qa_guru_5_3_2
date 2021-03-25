c7f8d9c842315c363281722683742a6a19a09200
anastasiya.krivokora <anastasiya.krivokora@quadcode.com>
2021-03-26 00:19:03 +0300

    add test

---
+++ src/test/java/gitHubTests.java
@@ -0,0 +1,29 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class gitHubTests {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    public void searchSoftAssertions() {
+        open("https://github.com/");
+        $("[name=q]").setValue("Selenide").pressEnter();
+        $("ul.repo-list li a").click();
+        $("h1").shouldHave(text("selenide / selenide"));
+        $("[data-tab-item='i4wiki-tab']").click();
+        $(byText("Soft assertions")).shouldBe(visible).click();
+        $("#wiki-content").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
